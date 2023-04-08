9bed31c7def9865c6017ed412a757664eb911733
Cole Perry <colecperry@gmail.com>
2023-04-08 12:15:10 -0700

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'White', 'Cool', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'Blue', 'Coo', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 11, 'F', 'Black', 'Cool', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 22, 'M', 'Green', 'Cool', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'White', 'Cool', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 19, 'M', 'Green', 'Cool', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 13, 'F', 'Yellow', 'Boo', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'White', 'Cool', True);
